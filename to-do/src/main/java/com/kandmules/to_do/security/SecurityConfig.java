7a716dd498ad550d4dc2ed03537b5e93ad2e1dc3
Amar Dattatri Kandmule <126358858+Amardattatri@users.noreply.github.com>
2024-12-11 12:35:37 +0530

    Update SecurityConfig.java

--- to-do/src/main/java/com/kandmules/to_do/security/SecurityConfig.java
+++ to-do/src/main/java/com/kandmules/to_do/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http    .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
-                .authenticated()
+                .antMatchers("/todos/**").authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
